8fec8f2d01532cfcf6b634b619e9a1647b27b30c
STaYsON <80705216+STaYsONzxc@users.noreply.github.com>
2023-09-13 06:04:42 +0700

    Subprocess Fix

---
+++ voice_changer/RVC/inferencer/OnnxRVCInferencerNono.py
@@ -0,0 +1,42 @@
+import torch
+import numpy as np
+from const import EnumInferenceTypes
+
+from voice_changer.RVC.inferencer.OnnxRVCInferencer import OnnxRVCInferencer
+
+
+class OnnxRVCInferencerNono(OnnxRVCInferencer):
+    def loadModel(self, file: str, gpu: int):
+        super().loadModel(file, gpu)
+        self.setProps(EnumInferenceTypes.onnxRVCNono, file, self.isHalf, gpu)
+        return self
+
+    def infer(
+        self,
+        feats: torch.Tensor,
+        pitch_length: torch.Tensor,
+        pitch: torch.Tensor | None,
+        pitchf: torch.Tensor | None,
+        sid: torch.Tensor,
+        convert_length: int | None,
+    ) -> torch.Tensor:
+        if self.isHalf:
+            audio1 = self.model.run(
+                ["audio"],
+                {
+                    "feats": feats.cpu().numpy().astype(np.float16),
+                    "p_len": pitch_length.cpu().numpy().astype(np.int64),
+                    "sid": sid.cpu().numpy().astype(np.int64),
+                },
+            )
+        else:
+            audio1 = self.model.run(
+                ["audio"],
+                {
+                    "feats": feats.cpu().numpy().astype(np.float32),
+                    "p_len": pitch_length.cpu().numpy().astype(np.int64),
+                    "sid": sid.cpu().numpy().astype(np.int64),
+                },
+            )
+
+        return torch.tensor(np.array(audio1))
---
+++ voice_changer/RVC/inferencer/RVCInferencerv2.py
@@ -0,0 +1,37 @@
+import torch
+from const import EnumInferenceTypes
+from voice_changer.RVC.deviceManager.DeviceManager import DeviceManager
+from voice_changer.RVC.inferencer.Inferencer import Inferencer
+from .rvc_models.infer_pack.models import SynthesizerTrnMs768NSFsid
+
+
+class RVCInferencerv2(Inferencer):
+    def loadModel(self, file: str, gpu: int):
+        self.setProps(EnumInferenceTypes.pyTorchRVCv2, file, True, gpu)
+
+        dev = DeviceManager.get_instance().getDevice(gpu)
+        isHalf = DeviceManager.get_instance().halfPrecisionAvailable(gpu)
+
+        cpt = torch.load(file, map_location="cpu")
+        model = SynthesizerTrnMs768NSFsid(*cpt["config"], is_half=isHalf)
+
+        model.eval()
+        model.load_state_dict(cpt["weight"], strict=False)
+
+        model = model.to(dev)
+        if isHalf:
+            model = model.half()
+
+        self.model = model
+        return self
+
+    def infer(
+        self,
+        feats: torch.Tensor,
+        pitch_length: torch.Tensor,
+        pitch: torch.Tensor,
+        pitchf: torch.Tensor,
+        sid: torch.Tensor,
+        convert_length: int | None,
+    ) -> torch.Tensor:
+        return self.model.infer(feats, pitch_length, pitch, pitchf, sid, convert_length=convert_length)
